3cae8706ae2fb47bcbad67f8e26953c2401e2d8a
Astapov, Pavel <p.astapov@itransition.com>
2023-05-22 20:32:56 +0300

    updated api

---
+++ src/api/generalinfo/routes/generalinfo.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * generalinfo router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::generalinfo.generalinfo');
---
+++ src/api/generalinfo/services/generalinfo.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * generalinfo service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::generalinfo.generalinfo');
